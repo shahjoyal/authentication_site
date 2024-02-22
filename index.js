a2d7133f5781fb29ba5c6df467f510ccde41d9a6
shahjoyal <132144905+shahjoyal@users.noreply.github.com>
2024-02-23 00:03:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "secrets",
-  password: "your password",
+  password: "Your Password",
   port: 5432,
 });
 db.connect();
